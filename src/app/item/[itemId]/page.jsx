7347bff1d68064dd19022f216bb70df84aa03d6a
Adrien KISSIE <fredkiss3@gmail.com>
2024-04-28 12:14:29 +0200

    ♻️ No more popcorn UI on SSR

--- src/app/item/[itemId]/page.jsx
+++ src/app/item/[itemId]/page.jsx
@@ -1,27 +1,43 @@
-import { cache } from 'react';
+import { headers } from "next/headers";
+import { cache } from "react";
+import * as React from "react";
 
 const items = [
-  { id: '1', name: 'item-one', detail: 'some detail for item one' },
-  { id: '2', name: 'item-two', detail: 'some detail for item two' },
-  { id: '3', name: 'item-three', detail: 'some detail for item three' },
-  { id: '4', name: 'item-four', detail: 'some detail for item four' },
-  { id: '5', name: 'item-five', detail: 'some detail for item five' },
+	{ id: "1", name: "item-one", detail: "some detail for item one" },
+	{ id: "2", name: "item-two", detail: "some detail for item two" },
+	{ id: "3", name: "item-three", detail: "some detail for item three" },
+	{ id: "4", name: "item-four", detail: "some detail for item four" },
+	{ id: "5", name: "item-five", detail: "some detail for item five" },
 ];
 
 const getData = cache(async (id) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({ data: items.find((item) => item.id === id) });
-    }, 500);
-  });
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve({ data: items.find((item) => item.id === id) });
+		}, 500);
+	});
 });
 
+function isSSRLoad() {
+	return headers().get("accept").includes("text/html");
+}
+
 export default async function Item({ params }) {
-  const response = await getData(params.itemId);
-  return (
-    <div>
-      <h1>{response.data.name}</h1>
-      <p>{response.data.detail}</p>
-    </div>
-  );
+	return isSSRLoad() ? (
+		<Content params={params} />
+	) : (
+		<React.Suspense fallback={<p>Loading...</p>}>
+			<Content params={params} />
+		</React.Suspense>
+	);
+}
+
+async function Content({ params }) {
+	const response = await getData(params.itemId);
+	return (
+		<div>
+			<h1>{response.data.name}</h1>
+			<p>{response.data.detail}</p>
+		</div>
+	);
 }
